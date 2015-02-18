5e40f610223db22d84d6ae3980bcbf020ac51f66
Dilyan Traykov <dtraykov94@gmail.com>
2015-02-18 23:21:15 +0200

    Just A Start

---
+++ Frogger/FroggerGame.cs
@@ -0,0 +1,25 @@
+﻿/*
+1. Include libraries (System, System.Collections.Generic...)
+2. Create the main struct for the objects (struct Object...)
+3. Start class FroggerGame and add main constants (HEIGHT, WIDTH, LIFES...)
+4. Create methods PrintObject(int x, int y, char c, int length, ConsoleColor color) and PrintText(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
+5. Display Welcome Screen Method - DisplayWelcomeMessage()
+6. Main Variables - lives, speed, symbols, colors...
+7. Initialization Method(s) - CreateObject(params[]) or (CreateFrog(params[]) and CreateCar(params[]))
+8. Main Game Loop - while(true) { ... }
+10. Check KeyPress Method - CheckKeyPress()
+11. Move Cars Method - MoveCars()
+12. Check For Collision Method - CheckForCollision()
+13. Check If Game Is over MEthod - CheckIfGameIsOver()
+14. Refresh Screen - Console.Clear();
+15. RestartGame() if hit or PrintObject(frog) and PrintObject(cars)
+15. Print Info Screen Method - PrintInfoScreen()
+16. Control Game Speed - Thread.Sleep(350 - (int)speed);
+ */
+
+using System;
+
+class FroggerGame
+{
+    
+}
