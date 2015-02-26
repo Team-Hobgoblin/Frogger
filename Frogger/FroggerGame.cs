9b048c9c85588238c55b3478c4296f2bdcfd2d71
Aleksandra Yanovska <a_s_q@mail.bg>
2015-02-26 12:01:29 +0200

    ...

--- Frogger/FroggerGame.cs
+++ Frogger/FroggerGame.cs
@@ -105,6 +105,7 @@ class FroggerGame
                     mrFrog.y,
                     'X',
                     ConsoleColor.Red);
+                GameOver();
             }
             PrintingString(41, 4, "Lives: " + mrFrogLives);
             // slow down the program - so we can see what is happening 
@@ -291,6 +292,7 @@ class FroggerGame
         Console.Clear();
         PrintStringArray(gameOver);
         Console.WriteLine("\n\n\n");
+        Console.ReadKey(true);
        
         
     }
